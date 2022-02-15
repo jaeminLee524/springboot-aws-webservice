4da9f153b9da7e0e9c2dec6ed9a772d5243cf153
jaeminLee524 <rkwoals524@hanmail.net>
2022-02-16 01:31:10 +0900

    게시글 test

---
+++ src/test/java/com/jaemin/book/springbootawswebservice/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,51 @@
+package com.jaemin.book.springbootawswebservice.domain.posts;
+
+import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+//@Transactional(readOnly = true) After method를 생략하고 @Transactional 을 이용해 테스트 가능
+public class PostsRepositoryTest {
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @After
+    public void cleanUp() {
+        postsRepository.deleteAll();
+    }
+
+//    @Transactional
+    @Test
+    public void 게시글저장_불러오기() {
+
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        //save data using the builder pattern
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("fkdlem524@naver.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        Assertions.assertThat(posts.getTitle()).isEqualTo(title);
+        Assertions.assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+}
