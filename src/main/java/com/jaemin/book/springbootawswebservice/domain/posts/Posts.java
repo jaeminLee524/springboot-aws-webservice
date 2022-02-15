98cbf37c4e11774559816527f1a912ceb50dc528
jaeminLee524 <rkwoals524@hanmail.net>
2022-02-16 00:42:16 +0900

    게시판 Entity

---
+++ src/main/java/com/jaemin/book/springbootawswebservice/domain/posts/Posts.java
@@ -0,0 +1,33 @@
+package com.jaemin.book.springbootawswebservice.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+//annotation의 순서는 주요 annotation을 클래스에 가깝게 배치
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    // 생성자에 포함된 필드만 builder에 포함
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
