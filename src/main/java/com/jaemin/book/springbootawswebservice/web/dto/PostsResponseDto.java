ea1a767c67c262e36e4e7d846b76fac53a489f8e
jaeminLee524 <rkwoals524@hanmail.net>
2022-02-17 01:43:05 +0900

    게시판 조회 responseDto

---
+++ src/main/java/com/jaemin/book/springbootawswebservice/web/dto/PostsResponseDto.java
@@ -0,0 +1,21 @@
+package com.jaemin.book.springbootawswebservice.web.dto;
+
+import com.jaemin.book.springbootawswebservice.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    // Entity -> Dto
+    public PostsResponseDto(Posts posts) {
+        this.id = posts.getId();
+        this.title = posts.getTitle();
+        this.content = posts.getContent();
+        this.author = posts.getAuthor();
+    }
+}
