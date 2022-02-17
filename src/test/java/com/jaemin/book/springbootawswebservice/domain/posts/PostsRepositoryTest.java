ddd1f368886b4a41d900f1da14040c1fe0e59781
jaeminLee524 <rkwoals524@hanmail.net>
2022-02-17 14:44:18 +0900

    공통정보매핑 클래스 테스트

--- src/test/java/com/jaemin/book/springbootawswebservice/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jaemin/book/springbootawswebservice/domain/posts/PostsRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -46,6 +47,30 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         Assertions.assertThat(posts.getTitle()).isEqualTo(title);
         Assertions.assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() throws Exception {
+
+        //given
+        LocalDateTime now = LocalDateTime.of(2022, 2, 17, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts findPost = postsList.get(0);
+
+        System.out.println(">>>>>>>>>>>> createDate= " + findPost.getCreatedDate() + "modifiedDate= " + findPost.getModifiedDate());
+
+        Assertions.assertThat(findPost.getCreatedDate()).isAfter(now);
+        Assertions.assertThat(findPost.getModifiedDate()).isAfter(now);
+
     }
 
 }
