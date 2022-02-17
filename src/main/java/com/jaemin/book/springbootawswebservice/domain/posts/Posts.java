a102d6e3a8b25c918d57986a81842c942da2c0dc
jaeminLee524 <rkwoals524@hanmail.net>
2022-02-17 14:42:23 +0900

    공통 정보 상속

--- src/main/java/com/jaemin/book/springbootawswebservice/domain/posts/Posts.java
+++ src/main/java/com/jaemin/book/springbootawswebservice/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.jaemin.book.springbootawswebservice.domain.posts;
 
+import com.jaemin.book.springbootawswebservice.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
