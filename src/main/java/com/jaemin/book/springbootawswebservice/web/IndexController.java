6bdaab9a6a0040a7395613c73814de51a7f05bc5
jaeminLee524 <rkwoals524@hanmail.net>
2022-02-18 11:23:12 +0900

    view conrtoller

---
+++ src/main/java/com/jaemin/book/springbootawswebservice/web/IndexController.java
@@ -0,0 +1,55 @@
+package com.jaemin.book.springbootawswebservice.web;
+
+import com.jaemin.book.springbootawswebservice.domain.service.posts.PostsService;
+import com.jaemin.book.springbootawswebservice.web.dto.PostsListResponseDto;
+import com.jaemin.book.springbootawswebservice.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+
+    /**
+     * 게시글 목록 화면
+     * return index.mustache
+     */
+    @GetMapping("/")
+    public String index(Model model) {
+        List<PostsListResponseDto> dtos = postsService.findAllDesc();
+        model.addAttribute("posts", dtos);
+        return "index";
+    }
+
+    /**
+     * 게시글 등록 화면
+     * return posts-save.mustache
+     */
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    /**
+     * 게시글 수정 화면
+     * call by index.mustache
+     * return posts-update.mustache
+     */
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto findPost = postsService.findById(id);
+
+        model.addAttribute("post", findPost);
+
+        return "posts-update";
+    }
+
+}
