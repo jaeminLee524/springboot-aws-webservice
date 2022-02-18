f786b3f609b302c799bd3486884428bb802c21af
jaeminLee524 <rkwoals524@hanmail.net>
2022-02-18 11:26:26 +0900

    postsRepository

--- src/main/java/com/jaemin/book/springbootawswebservice/domain/posts/PostsRepository.java
+++ src/main/java/com/jaemin/book/springbootawswebservice/domain/posts/PostsRepository.java
@@ -1,7 +1,13 @@
 package com.jaemin.book.springbootawswebservice.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 //Entity클래스와 기본 Repository는 함께 움직여야 하기에 도메인 패키지에서 관리
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id desc")
+    List<Posts> findALlDesc();
 }
