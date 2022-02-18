1db559455ed8362e6fafd396e48ae31518fc9543
jaeminLee524 <rkwoals524@hanmail.net>
2022-02-18 23:55:54 +0900

    SpringSecurityConfig

---
+++ src/main/java/com/jaemin/book/springbootawswebservice/config/auth/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.jaemin.book.springbootawswebservice.config.auth;
+
+import com.jaemin.book.springbootawswebservice.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/", "/css/**", "/images/**",
+                            "/js/**", "/h2-console/**").permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                    .anyRequest().authenticated()
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                        // OAuth2 로그인 성공 이후 사용자 정보를 가져올 때의 설정들을 담당한다.
+                        .userInfoEndpoint()
+                            // 소셜 로그인 성공 시 후속 조치를 진행할 UserService 의 구현체를 등록
+                            // 리소스 서버(소셜 서비스)에서 사용자 정보를 가져온 상태에서 추가로 진행하고자 하는 기능을 명시 가능
+                            .userService(customOAuth2UserService);
+    }
+}
