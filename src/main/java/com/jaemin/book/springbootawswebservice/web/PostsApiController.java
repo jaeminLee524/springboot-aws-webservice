14f3bac71e49c33457d2365df54901302bf7555e
jaeminLee524 <rkwoals524@hanmail.net>
2022-02-18 11:25:04 +0900

    게시글 crud api

--- src/main/java/com/jaemin/book/springbootawswebservice/web/PostsApiController.java
+++ src/main/java/com/jaemin/book/springbootawswebservice/web/PostsApiController.java
@@ -14,18 +14,34 @@ public class PostsApiController {
 
     private final PostsService postsService;
 
+    /**
+     * 게시글 저장 API
+     */    
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
 
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
+    /**
+     * 게시글 수정 API 
+     */
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
     }
 
-    @GetMapping("/api/v1/posts/{id}")
-    public PostsResponseDto findById(@PathVariable Long id) {
-        return postsService.findById(id);
+    /**
+     * 게시글 삭제 API 
+     */    
+    @DeleteMapping("/api/vi/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+
+        return id;
     }
 }
