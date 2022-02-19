9422263e5b8f3cedbe8ca8bbf055b50e8231ca98
jaeminLee524 <rkwoals524@hanmail.net>
2022-02-20 08:30:03 +0900

    add naver social login

--- src/main/java/com/jaemin/book/springbootawswebservice/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jaemin/book/springbootawswebservice/config/auth/dto/OAuthAttributes.java
@@ -4,9 +4,11 @@ import com.jaemin.book.springbootawswebservice.domain.user.Role;
 import com.jaemin.book.springbootawswebservice.domain.user.User;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
 
 import java.util.Map;
 
+@Log4j2
 @Getter
 public class OAuthAttributes {
     private Map<String, Object> attributes;
@@ -26,7 +28,28 @@ public class OAuthAttributes {
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName,
                                       Map<String, Object> attributes) {
+        for (Map.Entry<String, Object> entry : attributes.entrySet()) {
+            log.info(">>>>>>>>>>>>>>>>>>>>entry.getKey(), entry.getValue()", entry.getKey(), entry.getValue());
+        }
+
+
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("image_profile"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
