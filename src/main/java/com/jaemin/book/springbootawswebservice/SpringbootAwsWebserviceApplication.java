795029f825370602b1208df677adb39bb87e75d0
jaeminLee524 <rkwoals524@hanmail.net>
2022-02-20 09:31:04 +0900

    seperate the two settings

--- src/main/java/com/jaemin/book/springbootawswebservice/SpringbootAwsWebserviceApplication.java
+++ src/main/java/com/jaemin/book/springbootawswebservice/SpringbootAwsWebserviceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing //JPA Auditing 활성화
+//@EnableJpaAuditing //JPA Auditing 활성화, JpaConfig를 생성 후 분리
 @SpringBootApplication
 public class SpringbootAwsWebserviceApplication {
 
